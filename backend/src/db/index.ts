e060daad4f8486ef6410ef623ce6cfa0c0eb5909
Saad <mdsaad7323@gmail.com>
2026-06-20 01:14:59 +0400

    implemented clerk authentication and env. config.

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import{ drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 
-import * as schena from "./schema";
+import * as schema from "./schema";
 
 const pool = new pg.Pool({connectionString: process.env.DATABASE_URL});
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(pool, {schema});
