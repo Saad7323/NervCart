eaca6b86df16de1e3f7e4a5f357f08cbb6afb9a0
Saad <mdsaad7323@gmail.com>
2026-06-26 02:33:38 +0400

    integrated Sentry for error tracking and user context sync with clerk and made the initial layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
+import { Show, SignInButton, SignUpButton, UserButton, useAuth } from '@clerk/react';
+import PageLoader from './components/PageLoader';
+import Layout from './components/Layout';
+
 
 function App() {
+  const {isLoaded} = useAuth();
+  
+  if (!isLoaded) return <PageLoader />;
+
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal"/>
@@ -13,7 +19,9 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
+      <p className="text-red-700 text-3xl font-bold underline bg-amber-700">hello world</p>
+      <button className="btn btn-primary">Click me</button>
+    </Layout>
   );
 }
 
